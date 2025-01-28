d590988918e574eae909c217750f287e0e55548e
Maimuna <maimunaibrahim808@gmail.com>
2025-01-28 20:31:11 +0100

    fourth commit

--- typescript-tut/src/App.tsx
+++ typescript-tut/src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Heading } from "./components/Heading";
 import Status from "./components/Status";
-
+import { Oscar } from "./components/Oscar";
 function App() {
   return (
     <div className="App">
       <Status status="loading" />
-      <Heading/>
-      
+      <Heading>Hello World</Heading>
+    <Oscar>
+      <Heading>Oscar goes to Leonardo Dicpario!</Heading>
+    </Oscar>
     </div>
   );
 }
