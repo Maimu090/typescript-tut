f3e30a00d454ab59031809105c3cdb194686233e
Maimuna <maimunaibrahim808@gmail.com>
2025-02-21 16:14:37 +0100

    made some changes

--- typescript-tut/src/App.tsx
+++ typescript-tut/src/App.tsx
@@ -3,20 +3,23 @@ import { List } from "./components/generics/List";
 function App() {
     return (
         <div>
-       <List
-      items = {["Batman", "Superman", "Wonder Woman"]} onClick={(item) => console.log(item)} />
-      <List items = {[1, 2, 3]} onClick={(item) => console.log(item)} />
+       {/* <List
+      items = {["Batman", "Superman", "Wonder Woman"]} onClick={(item) => console.log(item)} /> */}
+      {/* <List items = {[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
       <List
       items = {[
         {
+            id: 1,
             first: "Bruce",
             last: "Wayne"
         }, 
         {
+            id: 2,
             first: "Clark",
             last: "Kent"
         }, 
         {
+            id: 3,
             first: 'Princess',
             last: 'Diana',
         },
